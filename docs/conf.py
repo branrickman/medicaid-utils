444820dc2a50ebfb31c599cfcaa7a57d39881c11
Bryan Brickman <61772752+branrickman@users.noreply.github.com>
2022-02-14 13:53:33 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.append(os.path.abspath('../../medicaid_utils'))
+sys.path.append(os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
